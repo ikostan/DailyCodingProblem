a534a43001e34c458de3dd0baed75d746a2519c3
ikostan <igorkostan@gmail.com>
2019-02-23 15:22:53 -0800

    unittest
    new unittest cases added

--- Problem1/test_find_numbers.py
+++ Problem1/test_find_numbers.py
@@ -9,8 +9,29 @@ class TestFindNumbers(unittest.TestCase):
         answer = [10, 7]
         self.assertListEqual(answer, find_numbers(goal, numbers))
 
-    def test_false(self):
+    def test_false_case(self):
         # given [10, 15, 3, 9] and k of 17, return False.
         goal = 17
         numbers = [10, 15, 3, 9]
         self.assertFalse(find_numbers(goal, numbers))
+
+    def test_negative_number(self):
+        # given [10, 15, 3, 7] and k of 17, return [10, -15] since 10 + (-15) is -5.
+        goal = -5
+        numbers = [10, -15, 3, 7]
+        answer = [10, -15]
+        self.assertListEqual(answer, find_numbers(goal, numbers))
+
+    def test_zero(self):
+        # given [10, 15, 3, 7] and k of 17, return [-15, 15] since 15 + (-15) is 0.
+        goal = 0
+        numbers = [10, -15, 3, 7, 15]
+        answer = [-15, 15]
+        self.assertListEqual(answer, find_numbers(goal, numbers))
+
+    def test_not_null(self):
+        # given [10, 15, 3, 7] and k of 17, return [-15, 15] since 15 + (-15) is 0.
+        goal = 0
+        numbers = [10, -15, 3, 7, 15]
+        answer = [-15, 15]
+        self.assertIsNotNone(find_numbers(goal, numbers))
