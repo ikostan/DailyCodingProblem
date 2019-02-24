f69675ca69ea513bfff863d96b0637e15825ea28
ikostan <igorkostan@gmail.com>
2019-02-23 16:27:29 -0800

    Problem #2
    Problem #2: solution + unit tests

--- Problem2/Problem2.py
+++ Problem2/Problem2.py
@@ -11,3 +11,26 @@ the expected output would be [2, 3, 6].
 
 Follow-up: what if you can't use division?
 '''
+
+def generate_array(input_array):
+    output_array = list()
+
+    for n in input_array:
+        i = input_array.index(n)
+        result = multiply_list_members(input_array, i)
+        print("For " + str(n) + " result number is " + str(result))
+        output_array.append(result)
+
+    print()
+
+    return output_array
+
+
+def multiply_list_members(input_array, i):
+    result = 1
+
+    for n in range(len(input_array)):
+        if n != i:
+            result *= input_array[n]
+
+    return result
---
+++ Problem2/test_generate_array.py
@@ -0,0 +1,24 @@
+import unittest
+import types
+from Problem2 import generate_array
+
+class TestGenerateArray(unittest.TestCase):
+    # verify that method does not return null
+    def test_not_null(self):
+        self.assertIsNotNone(generate_array(list()))
+
+    # if our input was [1, 2, 3, 4, 5], the expected output
+    # would be [120, 60, 40, 30, 24].
+    def test_basic_case_1(self):
+        input_array = [1, 2, 3, 4, 5]
+        output_array = [120, 60, 40, 30, 24]
+        self.assertListEqual(generate_array(input_array), output_array)
+
+    # If our input was [3, 2, 1],
+    # the expected output would be [2, 3, 6].
+    def test_basic_case_2(self):
+        input_array = [3, 2, 1]
+        output_array = [2, 3, 6]
+        self.assertListEqual(generate_array(input_array), output_array)
+
+
